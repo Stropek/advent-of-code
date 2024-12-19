7619dbcb2d68aaf5a8d15b765a372b117d573331
Stropek <curzytekp@gmail.com>
2024-12-19 01:20:43 +0100

    add day 16 / part 2 solution

--- solutions/2024/16/__main__.py
+++ solutions/2024/16/__main__.py
@@ -9,7 +9,6 @@ import regex as re
 from collections import Counter
 import math
 
-
 def solve(in_file_stream, out=sys.stdout):
     print('Solving!\n')
     total = 0
@@ -23,14 +22,16 @@ def solve(in_file_stream, out=sys.stdout):
     # start_x = 3
     # start_y = 11
         
-    start_path = (0, start_x, start_y, 'E', 0)
+    start_path = (0, start_x, start_y, 'E', 0, set())
     crossroads = dict()
         
     crossroads[0] = set((start_x, start_y))
     crossroads[-1] = dict()
     crossroads[-1][(start_x, start_y, 'E')] = 1000000
+    
+    path_all_final_moves = set()
         
-    print('Start: {} {}, dir: {}'.format(start_path[0], start_path[1], start_path[2]))
+    print('Start: {} {}, dir: {}'.format(start_path[1], start_path[2], start_path[3]))
         
     hm.display_matrix(maze)
 
@@ -39,14 +40,16 @@ def solve(in_file_stream, out=sys.stdout):
     # if maze[start_y][start_x + 1] == '.':
     #     paths[(start_x, start_y, 'E')] = [(start_x, start_y - 1, 'E', 1)]
         
-    (next_moves, min_moves) = find_next(start_path, crossroads, maze, min_moves)
+    (next_moves, min_moves, path_all_final_moves) = find_next(start_path, crossroads, maze, min_moves, path_all_final_moves)
+    
+    print('Start next moves: {}'.format(path_all_final_moves))
         
     print('-: {}'.format(next_moves))
-        
+
     while next_moves:
             
         it = 0
-        max_it = 100
+        max_it = 10000
             
         # print('Found next moves: {}'.format(next_moves))
             
@@ -56,8 +59,10 @@ def solve(in_file_stream, out=sys.stdout):
             next_move = next_moves.pop()
                 
             # print('Path {}: {} {}, dir: {}'.format(next_move[0], next_move[1], next_move[2], next_move[3]))
-            (next_for_path, min_moves) = find_next(next_move, crossroads, maze, min_moves)
+            (next_for_path, min_moves, path_all_final_moves) = find_next(next_move, crossroads, maze, min_moves, path_all_final_moves)
             # print('-: {}'.format(next_for_path))
+            
+            # print('Path for {}: {}'.format(next_move[0], sorted(next_move[5])))
             
             if len(next_for_path) == 0:
                 # print('Path {} reached a DEAD END at {} {}, dir: {}'.format(next_move[0], next_move[1], next_move[2], next_move[3]))
@@ -75,18 +80,30 @@ def solve(in_file_stream, out=sys.stdout):
                             
             # if it > max_it:
             #     break
-                
+                                
             it += 1
             
         break
-            
-    print('Total iterations: {}'.format(it))
+
+    # print(maze)
+
+    for final_move in path_all_final_moves:
+        fm_x = final_move[0]
+        fm_y = final_move[1]
         
+        maze[fm_y][fm_x] = 'O'
+ 
+    hm.display_matrix(maze)
+ 
+    print('Total iterations: {}'.format(it))
     print('Total: ' + str(min_moves))
+    
+    print('All final moves: {}'.format(sorted(path_all_final_moves)))
+    print('All final moves: {}'.format(len(path_all_final_moves)))
         
     hm.write_line(out, min_moves)
         
-def find_next(move, crossroads, maze, min_moves):
+def find_next(move, crossroads, maze, min_moves, path_all_final_moves):
     # print('Finding next for path: {}'.format(move))
         
     i = move[0]
@@ -94,6 +111,7 @@ def find_next(move, crossroads, maze, min_moves):
     y = move[2]
     direction = move[3]
     val = move[4]
+    path_moves = move[5]
     next_moves = []
     path_final_move = None
     
@@ -102,75 +120,109 @@ def find_next(move, crossroads, maze, min_moves):
     else:
         if crossroads[-1][(x, y, direction)] < val:
             print('Path {} already crossed ({}, {}) with a lower value {}'.format(i, x, y, crossroads[-1][(x, y, direction)]))
-            return ([], min_moves)
+            return ([], min_moves, path_all_final_moves)
         else:
             crossroads[-1][(x, y, direction)] = val
         
-    # print('Looking for next at ({}, {}), facing {}, with val {}'.format(x, y, direction, val))
+    # print('Path {} looking for next at ({}, {}), facing {}, with val {}'.format(i, x, y, direction, val))
         
     i_i = 0
     path_i = [i, len(crossroads), len(crossroads) + 1]
     
     # print('New paths: {}'.format(path_i))
+    new_path_moves = path_moves.copy()
+    new_path_moves.add((x, y))
             
     # check if E in current direction
     if direction == 'E' and maze[y][x + 1] == 'E':
-        path_final_move = (i, x + 1, y, 'E', val + 1)
+        new_path_moves.add((x + 1, y))
+        path_final_move = (i, x + 1, y, 'E', val + 1, new_path_moves)
     elif direction == 'W' and maze[y][x - 1] == 'E':
-        path_final_move = (i, x - 1, y, 'W', val + 1)
+        new_path_moves.add((x - 1, y))
+        path_final_move = (i, x - 1, y, 'W', val + 1, new_path_moves)
     elif direction == 'N' and maze[y - 1][x] == 'E':
-        path_final_move = (i, x, y - 1, 'N', val + 1)
+        new_path_moves.add((x, y - 1))
+        path_final_move = (i, x, y - 1, 'N', val + 1, new_path_moves)
     elif direction == 'S' and maze[y + 1][x] == 'E':
-        path_final_move = (i, x, y + 1, 'S', val + 1)
+        new_path_moves.add((x, y + 1))
+        path_final_move = (i, x, y + 1, 'S', val + 1, new_path_moves)
             
     if path_final_move:
-        print('Path {} reached the finish line at {}'.format(i, path_final_move))
+        # print('Path {} reached the finish line at {}'.format(i, path_final_move))
+        # print('\nBEFOFRE:')
+        # print(path_all_final_moves)
             
         if min_moves == -1:
             min_moves = path_final_move[4]
+            path_all_final_moves = new_path_moves.copy()
+            print('Setting path_all_final_moves to path_moves (len: {})'.format(len(new_path_moves)))
+            # print(path_all_final_moves)
         else:
             # print('Checking if {} is less than {}'.format(path_final_move[4], min_moves))
+            if (path_final_move[4] < min_moves):
+                print('Setting path_all_final_moves to path_moves (len: {})'.format(len(new_path_moves)))
+                path_all_final_moves = new_path_moves.copy()
+                print('Setting path_all_final_moves to path_moves (len: {})'.format(len(path_all_final_moves)))
+                # print(path_all_final_moves)
+            elif (path_final_move[4] == min_moves):
+                path_all_final_moves = path_all_final_moves.union(new_path_moves)
+                print('Adding to path_all_final_moves (len: {})'.format(len(path_all_final_moves)))
+                # print(path_all_final_moves)
+            
             min_moves = min(min_moves, path_final_move[4])
         
-        return ([], min_moves)
-
+        # print('AFTER:')
+        # print(path_all_final_moves)
+        return ([], min_moves, path_all_final_moves)
+    
     # check if can change direction
     if (x, y) in crossroads[i]:
         # print('Path {} crossing crossroad at ({}, {}) for a second time - DEAD END'.format(i, x, y))
-        return ([], min_moves)
-       
+        return ([], min_moves, path_all_final_moves)
+           
     if direction in ('E', 'W'):
         if maze[y - 1][x] == '.':
-            next_moves.append((path_i[i_i], x, y - 1, 'N', val + 1001))
+            # path_moves.add((x, y - 1))
+            next_moves.append((path_i[i_i], x, y - 1, 'N', val + 1001, new_path_moves))
             i_i += 1
         if maze[y + 1][x] == '.':
-            next_moves.append((path_i[i_i], x, y + 1, 'S', val + 1001))
+            # path_moves.add((x, y + 1))
+            next_moves.append((path_i[i_i], x, y + 1, 'S', val + 1001, new_path_moves))
             i_i += 1
             
     elif direction in ('N', 'S'):
         if maze[y][x - 1] == '.':
-            next_moves.append((path_i[i_i], x - 1, y, 'W', val + 1001))
+            # path_moves.add((x - 1, y))
+            next_moves.append((path_i[i_i], x - 1, y, 'W', val + 1001, new_path_moves))
             i_i += 1
         if maze[y][x + 1] == '.':
-            next_moves.append((path_i[i_i], x + 1, y, 'E', val + 1001))
+            # path_moves.add((x + 1, y))
+            next_moves.append((path_i[i_i], x + 1, y, 'E', val + 1001, new_path_moves))
             i_i += 1
         
     # check if can continue in current direction
     if direction == 'E' and maze[y][x + 1] == '.':
-        next_moves.append((path_i[i_i], x + 1, y, 'E', val + 1))
+        # path_moves.add((x + 1, y))
+        next_moves.append((path_i[i_i], x + 1, y, 'E', val + 1, new_path_moves))
     elif direction == 'W' and maze[y][x - 1] == '.':
-        next_moves.append((path_i[i_i], x - 1, y, 'W', val + 1))
+        # path_moves.add((x - 1, y))
+        next_moves.append((path_i[i_i], x - 1, y, 'W', val + 1, new_path_moves))
     elif direction == 'N' and maze[y - 1][x] == '.':
-        next_moves.append((path_i[i_i], x, y - 1, 'N', val + 1))
+        # path_moves.add((x, y - 1))
+        next_moves.append((path_i[i_i], x, y - 1, 'N', val + 1, new_path_moves))
     elif direction == 'S' and maze[y + 1][x] == '.':
-        next_moves.append((path_i[i_i], x, y + 1, 'S', val + 1))
+        # path_moves.add((x, y + 1))
+        next_moves.append((path_i[i_i], x, y + 1, 'S', val + 1, new_path_moves))
         
     # print('Number of different paths from {}: {}'.format(move, i_i + 1))
         
     if len(next_moves) > 3:
         print('More than 3 moves (impossible!) found at ({}, {})'.format(x, y))
         
+    # path_moves.add((x, y))
+    
     if len(next_moves) > 0:
+        
         if len(next_moves) > 1:
             # print('Found a CROSSROAD at ({}, {}) with {} next moves'.format(x, y, len(next_moves)))
             crossroads[i].add((x, y))
@@ -180,7 +232,7 @@ def find_next(move, crossroads, maze, min_moves):
     # else:
     #     print('Path {} reached a DEAD END at {} {}, dir: {}'.format(i, x, y, direction))
         
-    return (next_moves, min_moves)
+    return (next_moves, min_moves, path_all_final_moves)
         
 if __name__ == "__main__":
     
